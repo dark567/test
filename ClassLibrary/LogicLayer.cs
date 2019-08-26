47f6cedd1c269a07927b768afb4ca03c58fcca7e
dark567 <darkmit@list.ru>
2019-08-26 20:57:47 +0300

    first commit

---
+++ ClassLibrary/LogicLayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class LogicLayer
+    {
+        /// <summary>
+        /// какое то описание
+        /// </summary>
+        /// <returns></returns>
+        public static int[] AddArr()
+        {
+            Random rnd = new Random();
+            int[] arrT = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arrT[i] = rnd.Next(-100, 100);
+            }
+            return arrT;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ArrT"></param>
+        /// <returns></returns>
+        public static int SumAll(int[] ArrT)
+        {
+            int sum = 0;
+            foreach (var item in ArrT)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ArrT"></param>
+        /// <returns></returns>
+        public static int SumNeg(int[] ArrT)
+        {
+            int sum = 0;
+            foreach (var item in ArrT)
+            {
+                if (item < 0) sum += item;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using ClassLibrary1;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[10];
+
+
+            Arr = LogicLayer.AddArr();
+
+            for (int i = 0; i < Arr.GetLength(0); i++)
+            {
+                Console.WriteLine(Arr[i]);
+            }
+            Console.WriteLine($"Sum:{LogicLayer.SumAll(Arr)}");
+            Console.WriteLine($"Sum negative:{LogicLayer.SumNeg(Arr)}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
